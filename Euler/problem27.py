1539506236f8f9a2387df8d4b36c7dbe7fd0a36f
Xavi <xavi.rmz@gmail.com>
2010-01-02 01:50:29 -0500

    Solved Euler problem27

---
+++ Euler/problem27.py
@@ -0,0 +1,32 @@
+def isPrime(n):
+    if n <  2: return False
+    if n == 2: return True
+    if n % 2 == 0: return False
+    
+    i = 3
+    while(n / i >= i):
+        if(n % i == 0): return False
+        i += 2
+    
+    return True
+
+def testExpression(a, b):
+    for n in range(1, 900):
+        if not isPrime(n*n + a*n + b):
+            return (n, a, b)
+    
+    return (89, a, b)
+    
+maxSeq = (0, 0, 0)
+primes = [i for i in range(2, 1000) if isPrime(i)]
+
+for b in primes:
+    for a in range(-999, 1000):
+        seq = testExpression(a, b);
+        
+        if seq[0] > maxSeq[0]: maxSeq = seq
+        
+    
+    print("on b: %s" % b)
+
+print("maxSeq: %s", maxSeq)
